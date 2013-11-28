9500c64e20f20252b8e02b5e535d8d8728b9e954
Fabrice Ecaille <fecaille@viadeoteam.com>
2013-11-28 17:58:04 +0100

    Refactoring: move function to tools.js

--- js/tools.js
+++ js/tools.js
@@ -67,3 +67,17 @@ function angle(vector1, vector2) {
 		
 	return sign * alpha;
 }
+
+
+function getAliensMidHeight() {
+	var higherAlien = Math.max.apply( null, 
+		$(".alien").map(function() {
+			return $(this).y();
+		}).get() ),
+		lowerAlien = Math.min.apply( null, 
+		$(".alien").map(function() {
+			return $(this).y();
+		}).get() );
+		
+	return (higherAlien + lowerAlien) / 2;
+}
